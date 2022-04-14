e853320cc19abcaa01f9a1cf3bb73130e4d3537e
Nick Jurista <nick@inexact-scientist.com>
2022-04-13 23:27:18 -0400

    Not much here but whatever

---
+++ src/data/standard.js
@@ -0,0 +1,109 @@
+const standard = {
+  '1:1': {
+    description: 'Home Run',
+    bases: 4,
+    outs: 0
+  },
+  '1:2': {
+    description: 'Double',
+    bases: 2,
+    outs: 0
+  },
+  '1:3': {
+    description: 'Fly Out',
+    bases: 0,
+    outs: 1
+  },
+  '1:4': {
+    description: 'Walk',
+    bases: 1,
+    outs: 0
+  },
+  '1:5': {
+    description: 'Pop Out',
+    bases: 0,
+    outs: 1
+  },
+  '1:6': {
+    description: 'Single',
+    bases: 1,
+    outs: 0
+  },
+  '2:2': {
+    description: 'Double Play',
+    bases: 0,
+    outs: 2
+  },
+  '2:3': {
+    description: 'Ground Out',
+    bases: 0,
+    outs: 1
+  },
+  '2:4': {
+    description: 'Strike Out',
+    bases: 0,
+    outs: 1
+  },
+  '2:5': {
+    description: 'Single',
+    bases: 1,
+    outs: 0
+  },
+  '2:6': {
+    description: 'Strike Out',
+    bases: 0,
+    outs: 1
+  },
+  '3:3': {
+    description: 'Walk',
+    bases: 1,
+    outs: 0
+  },
+  '3:4': {
+    description: 'Triple',
+    bases: 3,
+    outs: 0
+  },
+  '3:5': {
+    description: 'Ground Out',
+    bases: 0,
+    outs: 1
+  },
+  '3:6': {
+    description: 'Fly Out',
+    bases: 0,
+    outs: 1
+  },
+  '4:4': {
+    description: 'Walk',
+    bases: 1,
+    outs: 0
+  },
+  '4:5': {
+    description: 'Pop Out',
+    bases: 0,
+    outs: 1
+  },
+  '4:6': {
+    description: 'Strike Out',
+    bases: 0,
+    outs: 1
+  },
+  '5:5': {
+    description: 'Double',
+    bases: 2,
+    outs: 0
+  },
+  '5:6': {
+    description: 'Sacrifice Fly',
+    bases: 1,
+    outs: 1
+  },
+  '6:6': {
+    description: 'Home Run',
+    bases: 4,
+    outs: 0
+  },
+};
+
+export { standard };
