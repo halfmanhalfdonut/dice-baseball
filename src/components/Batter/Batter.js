2c46f06f1a6dfc4f1569b8a9d2ae4f1ef040214f
Nick Jurista <nick@inexact-scientist.com>
2022-04-17 13:17:04 -0400

    Adding some functionality

--- src/components/Batter/Batter.js
+++ src/components/Batter/Batter.js
@@ -3,6 +3,30 @@ import { standard } from '../../data/standard.js';
 class Batter extends HTMLElement {
   constructor() {
     super();
+
+    this.currentRoll;
+  }
+
+  removeListeners = () => {
+    document.removeEventListener('dice:roll', this.handleDiceRoll);
+  }
+
+  updateUI = () => {
+    document.getElementById(this.currentRoll)?.classList?.toggle('active-roll');
+  }
+
+  handleDiceRoll = ({ detail }) => {
+    const { roll } = detail;
+    
+    this.updateUI();
+    this.currentRoll = `dice-${roll}`;
+    this.updateUI();
+
+    document.dispatchEvent('dice:roll:details', {
+      detail: {
+        data: standard[roll]
+      }
+    });
   }
 
   connectedCallback() {
@@ -19,6 +43,7 @@ class Batter extends HTMLElement {
       }
 
       const div = document.createElement('div');
+      div.setAttribute('id', `dice-${key}`);
       div.setAttribute('class', 'dice-roll');
       div.innerHTML = `<span class="dice">${entry.dice}</span> ${entry.description}`;
 
@@ -28,6 +53,8 @@ class Batter extends HTMLElement {
     wrappers.forEach(wrapper => {
       this.appendChild(wrapper);
     });
+
+    document.addEventListener('dice:roll', this.handleDiceRoll);
   }
 }
 
--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,19 +1,64 @@
 class Controls extends HTMLElement {
   constructor() {
     super();
+
+    this.counter = 0;
+    this.diceMapping = [ null, '⚀', '⚁', '⚂', '⚃', '⚄', '⚅', ];
+  }
+
+  removeListeners = () => {
+    this.button?.removeEventListener('pointerup', this.handleRoll);
+  }
+
+  rollDice = () => {
+    this.counter++;
+
+    const lowValue = 1;
+    const highValue = 6;
+    const one = ~~(Math.random() * highValue) + lowValue;
+    const two = ~~(Math.random() * highValue) + lowValue;
+
+    this.tray.innerHTML = `${this.diceMapping[one]} ${this.diceMapping[two]}`;
+
+    if (this.counter === 10) {
+      document.dispatchEvent(new CustomEvent('dice:roll', {
+        detail: {
+          roll: `${Math.min(one, two)}:${Math.max(one, two)}`
+        }
+      }));
+
+      // reset counter
+      this.counter = 0;
+    }
   }
 
   handleRoll = () => {
-    // nothing yet
+    for (let i = 0; i < 10; i++) {
+      setTimeout(this.rollDice, 250 * i);
+    }
   }
 
   connectedCallback() {
+    this.removeListeners();
+    
+    const wrapper = document.createElement('section');
+    wrapper.setAttribute('class', 'controls');
+
     const button = document.createElement('button');
     button.setAttribute('class', 'pitch');
-    button.innerText = 'Pitch';
-    button.onclick = this.handleRoll;
+    button.innerText = '⚾ Roll';
+    button.addEventListener('pointerup', this.handleRoll);
+
+    const tray = document.createElement('div');
+    tray.setAttribute('class', 'tray');
+    tray.innerHTML = '&nbsp;';
+
+    this.tray = tray;
     
-    this.appendChild(button);
+    wrapper.appendChild(tray);
+    wrapper.appendChild(button);
+
+    this.appendChild(wrapper);
   }
 }
 
