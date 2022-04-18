45a0fe2db64630e54bd322c045f697326aec6ff9
Nick Jurista <nick@inexact-scientist.com>
2022-04-17 21:02:27 -0400

    removing extra Outs

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ export * from './Batter/Batter.js';
 export * from './Controls/Controls.js';
 export * from './Field/Field.js';
 export * from './Game/Game.js';
-export * from './Outs/Outs.js';
 export * from './Scorebox/Scorebox.js';
