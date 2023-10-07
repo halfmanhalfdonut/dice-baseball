8a15c8165ae71ca93d46d554448e7ebdbe3b1600
Nick Jurista <nick@inexact-scientist.com>
2023-10-07 10:31:41 -0400

    moving last bit of css

--- src/custom-elements/Game/Game.js
+++ src/custom-elements/Game/Game.js
@@ -1,8 +1,49 @@
 import GameEngine from '../../services/GameEngine/GameEngine.js';
+import { addStyles } from '../../services/Utils/Utils.js';
 
 class Game extends HTMLElement {
   constructor() {
     super();
+
+    let styles = `
+      db-game {
+        display: flex;
+        align-items: center;
+        box-sizing: border-box;
+        width: 100vw;
+        height: 100vh;
+        padding: 0;
+        background: #a0c526;
+        background: radial-gradient(circle, #a0c526 9%, #4a6717 100%);
+      }
+
+      .game {
+        margin: 0 auto;
+        width: 100vw;
+        box-sizing: border-box;
+        text-align: center;
+        background: var(--outline);
+      }
+
+      @media screen and (min-width: 768px) {
+        db-game {
+          padding: 4px;
+        }
+      
+        .game {
+          width: 75vw;
+          padding: 5px;
+        }
+      }
+      
+      @media screen and (min-width: 1024px) {
+        .game {
+          width: 55vw;
+        }
+      }
+    `;
+
+    addStyles(styles, 'db-game');
     
     GameEngine.setup();
   }
